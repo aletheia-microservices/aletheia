c9035191a4c5e053c8f574699a64eef69ec75b4e
Mafalda Ferreira <mafaldasofiacf@gmail.com>
2025-09-25 09:47:33 +0100

    feat(eval): plots for analysis time (and #ms, #ds) per application

--- eval/stacked_bars_time_per_complexity.py
+++ eval/stacked_bars_time_per_complexity.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 apps = ["Digota", "Sockshop", "PostNotification", "SocialNetwork", "MediaMicroservices", "TrainTicket"]
@@ -8,50 +9,77 @@ ms_counts = np.array([4, 7, 3, 10, 5, 30])
 ds_counts = np.array([5, 6, 2, 10, 4, 21])
 complexity = ms_weight * ms_counts + ds_weight * ds_counts
 
-parsing_time   = np.array([10.963, 10.689, 7.861, 17.403, 8.688, 30.954])
-schema_time = np.array([0.10, 0.090, 0.007, 0.237, 0.40, 0.371])
-detection_time = np.array([0.012, 0.061, 0.005, 0.142, 0.032, 0.376])
+#parsing_time   = np.array([10.963, 10.689, 7.861, 17.403, 8.688, 30.954])
+#schema_time = np.array([0.10, 0.090, 0.007, 0.237, 0.40, 0.371])
+#detection_time = np.array([0.012, 0.061, 0.005, 0.142, 0.032, 0.376])
 
 #order = np.argsort(complexity)
 # use lexsort: primary = complexity, secondary = ms_counts (descending)
-order = np.lexsort((ds_counts, complexity))
+
+parsing_time   = np.array([10.96, 10.69, 7.86, 17.40, 8.69, 30.95]) #s
+schema_time    = np.array([100, 90, 7, 237, 400, 371]) #ms
+detection_time = np.array([12, 61, 5, 142, 32, 376]) #ms
+
+parsing_time = np.round(parsing_time).astype(int)
+
+# sort apps by complexity
+order = np.lexsort((ms_counts, complexity))
 
 apps_sorted      = [apps[i] for i in order]
 parsing_sorted   = parsing_time[order]
+schema_sorted    = schema_time[order]
 detection_sorted = detection_time[order]
-schema_sorted = schema_time[order]
 
 x = np.arange(len(apps_sorted))
 
-plt.figure(figsize=(9,9)) 
+BASE_COLOR_PALETTE = sns.color_palette('deep', 12)
+COLORS = {
+  'parser': BASE_COLOR_PALETTE[0],
+  'schema': BASE_COLOR_PALETTE[1],
+  'detector': BASE_COLOR_PALETTE[2],
+}
 
-parse_color, detect_color = "#80cdc1", "#f4a582"
-parse_color, detect_color = "#fdbf6f", "#cab2d6"
-parse_color, detect_color = "#a6cee3", "#b2df8a"
-parse_color, detect_color = "#9ecae1", "#fdd0a2"
-parse_color, detect_color = "#1f77b4", "#ff7f0e"
-parse_color, detect_color = "#2ca02c", "#d62728"
-parse_color, detect_color = "#8c564b", "#e377c2"
-parse_color, detect_color = "#7f7f7f", "#bcbd22"
-parse_color, detect_color = "#5db8ea", "#fd9144"
-parse_color, schema_color, detect_color = "#5db8ea", "#fd9144", "#9467bd"
+xtick_labels = [f"{app}" for app, c in zip(apps_sorted, complexity[order])]
 
-# -------------------------------
+sns.set_theme(style='ticks')
+plt.rcParams['figure.dpi'] = 600
+plt.rcParams['figure.figsize'] = [5, 5]
+plt.rcParams['axes.labelsize'] = 'xx-small'
+plt.rcParams['legend.fontsize'] = 'xx-small'
+plt.rcParams['xtick.labelsize'] = 'xx-small'
+plt.rcParams['ytick.labelsize'] = 'xx-small'
 
-bars_parse   = plt.bar(x, parsing_sorted, label="Code Parser -> Abstract Graph (Tainted)", color=parse_color)
-bars_schema  = plt.bar(x, schema_sorted, bottom=parsing_sorted, label="Schema Builder", color=schema_color)
-bars_detect  = plt.bar(x, detection_sorted, bottom=parsing_sorted+schema_sorted, label="Pattern Detector", color=detect_color)
+fig, axes = plt.subplots(3, 1, sharex=True)
 
-xtick_labels = [f"{app}\n({c})" for app, c in zip(apps_sorted, complexity[order])]
+bars0 = axes[0].bar(x, parsing_sorted, color=COLORS['parser'])
+axes[0].bar_label(bars0, fmt="%ds", fontsize=6)
+axes[0].set_title("Parsing", fontsize=8)
+axes[0].set_ylabel("Time (s)")
+axes[0].set_ylim(0, 40)
+
+bars1 = axes[1].bar(x, schema_sorted, color=COLORS['schema'])
+axes[1].bar_label(bars1, fmt="%dms", fontsize=6)
+axes[1].set_title("Schema Building", fontsize=8)
+axes[1].set_ylabel("Time (ms)")
+axes[1].set_ylim(0, 500)
+
+bars2 = axes[2].bar(x, detection_sorted, color=COLORS['detector'])
+axes[2].bar_label(bars2, fmt="%dms", fontsize=6)
+axes[2].set_title("Detection", fontsize=8)
+axes[2].set_ylabel("Time (ms)")
+axes[2].set_ylim(0, 500)
+
 plt.xticks(x, xtick_labels, rotation=20, ha="right")
 
-plt.ylabel("Time (s)")
-plt.title("Analysis Time per Application (sorted by complexity)")
-plt.legend()
+# shared xx
+#axes[-1].set_xlabel("Application complexity")
 
-totals = parsing_sorted + detection_sorted
-for xi, total in zip(x, totals):
-    plt.text(xi, total + max(totals)*0.015, f"{total:.3f}s", ha="center", va="bottom", fontsize=8)
+# shared yy (smaller means further away from axis)
+#fig.supylabel("Time", fontsize=9, x=0.02)
 
 plt.tight_layout()
-plt.show()
+
+# smaller means left border
+plt.subplots_adjust(left=0.12)
+plt.savefig("plot-time-complexity.png")
+#plt.show()
